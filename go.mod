b34a90925c10933d6e2cfdffdf28085a7838c21d
Dimitris Zervas <dzervas@dzervas.gr>
2020-05-22 11:13:38 +0300

    Space changes (?)

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/dzervas/netcatty
+
+go 1.13
+
+require (
+	github.com/amoghe/distillog v0.0.0-20180726233512-ae382b35b717
+	github.com/jessevdk/go-flags v1.4.0
+	github.com/kr/pty v1.1.4
+	github.com/logrusorgru/aurora v0.0.0-20181002194514-a7b3b318ed4e
+	github.com/mattn/go-colorable v0.1.1
+	github.com/mattn/go-isatty v0.0.7
+	github.com/mattn/go-tty v0.0.0-20190407112021-83fae09cc007
+	github.com/mingrammer/cfmt v1.1.0
+	golang.org/x/sys v0.0.0-20190405154228-4b34438f7a67
+)
--- main.go
+++ main.go
@@ -8,12 +8,12 @@ import (
 	"os/signal"
 	"strings"
 
-	"github.com/dzervas/netcatty/service"
-	"github.com/dzervas/netcatty/inout"
 	"github.com/dzervas/netcatty/action"
+	"github.com/dzervas/netcatty/inout"
+	"github.com/dzervas/netcatty/service"
 	// "github.com/dzervas/netcatty/ui"
 
-	"github.com/dzervas/mage"
+	//"github.com/dzervas/mage"
 	"github.com/jessevdk/go-flags"
 	"github.com/mingrammer/cfmt"
 )
@@ -44,51 +44,51 @@ var opts struct {
 	// Gateway		[]string	`short:"g" long:"gateway"       description:"Source-routing hop point[s], up to 8"`
 	// TODO: WTF is this?
 	// Pointer		int			`short:"G" long:"pointer"       description:"Source-routing pointer:4, 8, 12, ..."`
-	Listen		bool		`short:"l" long:"listen"        description:"Listen mode, for inbound connects"`
+	Listen bool `short:"l" long:"listen"        description:"Listen mode, for inbound connects"`
 	// TODO: Needs exposure of listen/dial conns
 	// DontResolve	bool		`short:"n" long:"dont-resolve"  description:"Numeric-only IP addresses, no DNS"`
-	Port		string		`short:"p" long:"local-port"    description:"Local port number"`
+	Port string `short:"p" long:"local-port"    description:"Local port number"`
 	// Does not have any effect - even on netcat
-	Randomize	bool		`short:"r" long:"randomize"     description:"Randomize local and remote ports"`
-	NoRaw		bool		`short:"R" long:"no-raw"        description:"Do NOT put TTY in Raw mode"`
-	Source		string		`short:"s" long:"source"        description:"Local source address (ip or hostname)"`
+	Randomize bool   `short:"r" long:"randomize"     description:"Randomize local and remote ports"`
+	NoRaw     bool   `short:"R" long:"no-raw"        description:"Do NOT put TTY in Raw mode"`
+	Source    string `short:"s" long:"source"        description:"Local source address (ip or hostname)"`
 	// TODO
-	Telnet		bool		`short:"T" long:"telnet"        description:"answer using TELNET negotiation"`
+	Telnet bool `short:"T" long:"telnet"        description:"answer using TELNET negotiation"`
 	// TODO
-	Verbose		bool		`short:"v" long:"verbose"       description:"-- Not effective, backwards compatibility"`
+	Verbose bool `short:"v" long:"verbose"       description:"-- Not effective, backwards compatibility"`
 	// TODO
-	Version		bool		`short:"V" long:"version"       description:"Output version information and exit"`
+	Version bool `short:"V" long:"version"       description:"Output version information and exit"`
 	// TODO: Needs exposure of listen/dial conns
 	// Wait		int			`short:"w" long:"wait"          description:"Timeout for connects and final net reads"`
 
 	Positional struct {
-		Hostname	string	`positional-arg-name:"hostname"`
-		Port		string	`positional-arg-name:"port"`
+		Hostname string `positional-arg-name:"hostname"`
+		Port     string `positional-arg-name:"port"`
 	} `positional-args:"yes"`
 }
 
 var optsService struct {
-	Protocol	string		`short:"P" long:"protocol"      description:"Provide protocol in the form of tcp{,4,6}|udp{,4,6}|unix{,gram,packet}|ip{,4,6}:[<protocol-number>|<protocol-name>]\nFor <protocol-number> check https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml"`
-	TCP			bool		`short:"t" long:"tcp"           description:"TCP mode (default)"`
-	UDP			bool		`short:"u" long:"udp"           description:"UDP mode"`
-	Mage		int			`long:"mage"                    description:"Use the mage protocol over the selected service, on the specified channel" default:"-1"`
+	Protocol string `short:"P" long:"protocol"      description:"Provide protocol in the form of tcp{,4,6}|udp{,4,6}|unix{,gram,packet}|ip{,4,6}:[<protocol-number>|<protocol-name>]\nFor <protocol-number> check https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml"`
+	TCP      bool   `short:"t" long:"tcp"           description:"TCP mode (default)"`
+	UDP      bool   `short:"u" long:"udp"           description:"UDP mode"`
+	//Mage     int    `long:"mage"                    description:"Use the mage protocol over the selected service, on the specified channel" default:"-1"`
 }
 
 var optsInOut struct {
 	// This is already the behaviour. Probably too difficult (and no reason) to implement
 	// Close		bool		`short:"c" long:"close"         description:"Close connection on EOF from stdin"`
-	Exec		string		`short:"e" long:"exec"          description:"Program to exec after connect"`
-	Interval	int			`short:"i" long:"interval"      description:"Delay interval for data sent, ports scanned"`
+	Exec     string `short:"e" long:"exec"          description:"Program to exec after connect"`
+	Interval int    `short:"i" long:"interval"      description:"Delay interval for data sent, ports scanned"`
 	// TODO: Needs exposure of listen/dial conns
 	// Tunnel		string		`short:"L" long:"tunnel"        description:"Forward local port to remote address"`
 	// TODO
-	Output		string		`short:"o" long:"output"        description:"Output hexdump traffic to FILE (implies -x)"`
-	HexDump		bool		`short:"x" long:"hexdump"       description:"Hexdump incoming and outgoing traffic"`
-	Zero		bool		`short:"z" long:"zero"          description:"Zero-I/O mode (used for scanning)"`
+	Output  string `short:"o" long:"output"        description:"Output hexdump traffic to FILE (implies -x)"`
+	HexDump bool   `short:"x" long:"hexdump"       description:"Hexdump incoming and outgoing traffic"`
+	Zero    bool   `short:"z" long:"zero"          description:"Zero-I/O mode (used for scanning)"`
 }
 
 var optsAction struct {
-	Detect		bool		`short:"D" long:"detect"        description:"Detect remote shell automatically and try to raise a TTY on the remote" group:"Actions"`
+	Detect bool `short:"D" long:"detect"        description:"Detect remote shell automatically and try to raise a TTY on the remote" group:"Actions"`
 }
 
 func main() {
@@ -101,7 +101,9 @@ func main() {
 	parser.AddGroup("Action", "", &optsAction)
 
 	_, err := parser.Parse()
-	if err != nil { return }
+	if err != nil {
+		return
+	}
 
 	// netcat behaviour where to connect you do `nc <ip> <port>`
 	// but to listen you do `nc -lp <port>`
@@ -161,15 +163,21 @@ func main() {
 		}
 	}
 
-	if optsInOut.HexDump { ioc = &inout.HexDump{ioc} }
-	if optsInOut.Interval > 0 { ioc = &inout.Interval{ioc, optsInOut.Interval} }
+	if optsInOut.HexDump {
+		ioc = &inout.HexDump{ioc}
+	}
+	if optsInOut.Interval > 0 {
+		ioc = &inout.Interval{ioc, optsInOut.Interval}
+	}
 
 	// Service
 	var s service.Server
 	s = service.NewNet(ioc)
 
 	// Actions
-	if optsAction.Detect { action.NewRaiseTTY(s).Register() }
+	if optsAction.Detect {
+		action.NewRaiseTTY(s).Register()
+	}
 
 	// Main Loop
 	var conn net.Conn
@@ -186,14 +194,14 @@ func main() {
 	// go ui.UIMain()
 
 	// Mage Protocol
-	if optsService.Mage >= 0 {
-		stream := &mage.Stream{
-			Reader: conn,
-			Writer: conn,
-		}
-		readwriter := stream.GetReadWriter(uint8(optsService.Mage))
-		s.ProxyLoop(readwriter, readwriter)
-	} else {
+	//if optsService.Mage >= 0 {
+	//	stream := &mage.Stream{
+	//		Reader: conn,
+	//		Writer: conn,
+	//	}
+	//	readwriter := stream.GetReadWriter(uint8(optsService.Mage))
+	//	s.ProxyLoop(readwriter, readwriter)
+	//} else {
 		s.ProxyLoop(conn, conn)
-	}
+	//}
 }
